beed938ff754f20fc6d082e03a4fbdb7c988a7f1
Edilson Moizinho <edilsondiasalves@outlook.com>
2020-08-05 16:25:17 -0300

    Removendo import nao usado

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,5 +1,5 @@
 import { StorageService } from './../storage.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from '../../config/api.config';
 import { Observable } from 'rxjs/Rx';
