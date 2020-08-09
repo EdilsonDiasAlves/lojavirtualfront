86d7a2556695cb794af3b9173b88f92bcfb4578b
Edilson Moizinho <edilsondiasalves@outlook.com>
2020-08-09 16:56:16 -0300

    Removendo imports nao utilizados

--- src/pages/signup/signup.module.ts
+++ src/pages/signup/signup.module.ts
@@ -1,6 +1,5 @@
 import { CidadeService } from './../../services/domain/cidade.service';
 import { EstadoService } from './../../services/domain/estado.service';
-import { CategoriaService } from './../../services/domain/categoria.service';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SignupPage } from './signup';
