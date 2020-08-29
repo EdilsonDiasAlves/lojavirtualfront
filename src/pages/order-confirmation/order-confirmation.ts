880529e0bd89e50dcb006a3303b52011aa21fec5
Edilson Moizinho <edilsondiasalves@outlook.com>
2020-08-29 14:02:18 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -53,7 +54,7 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codPedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if(error.status == 403){
@@ -64,6 +65,15 @@ export class OrderConfirmationPage {
 
   back() {
     this.navCtrl.setRoot('CartPage');
+  }
+
+  categorias() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  private extractId(location: string) {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length)
   }
 
 }
