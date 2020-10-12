c05b653a02a27a487bd82dda525ea990a5b5c8a1
Edilson Moizinho <edilsondiasalves@outlook.com>
2020-10-11 21:58:38 -0300

    Correcoes para rodar o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: "http://localhost:8080",
+  //baseUrl: "http://localhost:8080",
+  baseUrl: "https://loja-virtual-pwa.herokuapp.com",
   bucketBaseUrl: "https://loja-virtual-pwa.s3-sa-east-1.amazonaws.com"
 }
